b189b0e06870c1912316723bf7736e25b0472a04
Mostafa Nabieh <engaymanfarag1997@gmail.com>
2023-11-07 21:37:26 +0200

    Create list product view and serialzer product

--- products/models.py
+++ products/models.py
@@ -14,8 +14,11 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     descreption = models.TextField(max_length=1000)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rateings = models.DecimalField(max_digits=1, decimal_places=1)
+    rateings = models.DecimalField(max_digits=3, decimal_places=1, default=0)
     stock = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, null=True,on_delete=models.SET_NULL)
     category = models.ForeignKey(Category, null=True,on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
